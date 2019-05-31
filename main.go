0dba080f22fecfaa6a5cf4d6a4afaaad73d9a386
Marcela Sisiliani de Sena Silva <msilva@band.com.br>
2019-05-31 18:52:34 -0300

    Insert code to send new messages to RabbitMQ Queue

--- main.go
+++ main.go
@@ -1,9 +1,52 @@
-package main 
+package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
 )
 
 func main() {
 	fmt.Println("Init server")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	//Create the queue
+	q, err := ch.QueueDeclare(
+		"Hello World Queue", //name
+		false,               // durable
+		false,               // delete when unused
+		false,               // exclusive
+		false,               // no-wait
+		nil,                 // arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World Go-RabbitMQ 2"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+
+	failOnError(err, "Failed to publish a message")
+
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
